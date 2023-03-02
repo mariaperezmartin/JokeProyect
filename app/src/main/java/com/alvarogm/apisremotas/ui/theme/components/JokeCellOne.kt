15e47d780b61706e00dac9299bffb7081003ddb5
AlvaroGM <alvarogm@iescastelar.com>
2023-03-02 01:37:16 +0100

    Code cleaning and error checking

--- app/src/main/java/com/alvarogm/apisremotas/ui/theme/components/JokeCellOne.kt
+++ app/src/main/java/com/alvarogm/apisremotas/ui/theme/components/JokeCellOne.kt
@@ -1,26 +1,20 @@
 package com.alvarogm.apisremotas.ui.theme.components
 
-import androidx.annotation.DrawableRes
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.ArrowBack
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.alvarogm.apisremotas.R
-import com.alvarogm.apisremotas.data.remote.Joke
-import com.alvarogm.apisremotas.data.remote.JokeClass
+import com.alvarogm.apisremotas.data.local.database.Jokes
+import com.alvarogm.apisremotas.data.local.preferences.StoreUserLanguage
 import com.alvarogm.apisremotas.presentation.JokesViewModel
 import com.alvarogm.apisremotas.ui.theme.AppColors
 import com.alvarogm.apisremotas.ui.theme.navigation.Destinations
@@ -31,51 +25,70 @@ import com.gandiva.neumorphic.neu
 import com.gandiva.neumorphic.shape.Flat
 import com.gandiva.neumorphic.shape.Oval
 import com.gandiva.neumorphic.shape.RoundedCorner
+import com.mathroda.snackie.BrightGreen
+import com.mathroda.snackie.SnackieError
+import com.mathroda.snackie.SnackieSuccess
+import com.mathroda.snackie.rememberSnackieState
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
-
 @Composable
-fun JokeCellOne(joke: Joke, viewModel: JokesViewModel) {
-    val scaffoldState = rememberScaffoldState()
-    val snackbarCoroutineScope = rememberCoroutineScope()
+fun JokeCellOne(
+    type: String,
+    delivery:  String,
+    setup:  String,
+    joke: String,
+    category: String,
+    viewModel: JokesViewModel,
+) {
+    val success = rememberSnackieState()
+    val error = rememberSnackieState()
+    val mContext = LocalContext.current
+    val dataStore = StoreUserLanguage(mContext)
+    val savedEmail = dataStore.getLanguage.collectAsState(initial = "")
+
+    SnackieSuccess(state = success, containerColor = BrightGreen)
+    SnackieError(state = error)
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(10.dp,0.dp,0.dp,10.dp)
+            .padding(0.dp,0.dp,0.dp,10.dp)
             .neu(
                 lightShadowColor = AppColors.lightShadow(),
                 darkShadowColor = AppColors.darkShadow(),
                 shadowElevation = defaultElevation,
-                shape = Flat(RoundedCorner(24.dp)),
+                shape = Flat(RoundedCorner(20.dp)),
                 lightSource = LightSource.LEFT_TOP,
             ),
         elevation = 0.dp,
         shape = RoundedCornerShape(24.dp),
     ) {
+
         Row(modifier = Modifier.fillMaxSize()) {
             Column(
                 modifier = Modifier
                     .weight(3f)
-                    .padding(start = 17.dp, top = 0.dp, end = 0.dp, bottom = 0.dp)
+                    .padding(18.dp)
             ) {
 
-                if (joke.type == "twopart") {
+                if (type == "twopart") {
                     Text(
-                        text = joke.lang + " - " + joke.category,
-                        style = TextStyle(color = Color(0xFF87CEFA))
+                        text = savedEmail.value.toString() + " - " + category,
+                        style = TextStyle(color = Color(0xFF1393E2))
                     )
-                    Text(text = joke.setup)//pregunta
-                    Text(text = joke.delivery, style = TextStyle(color = Color.Red))//respuesta
+                    Text(text = setup)//pregunta
+                    Text(text = delivery, style = TextStyle(color = Color.Red),fontSize = 19.sp)//respuesta
 
                 } else {
                     Text(
-                        text = joke.lang + " - " + joke.category,
-                        style = TextStyle(color = Color(0xFF87CEFA))
+                        text = savedEmail.value.toString() + " - " + category,
+                        style = TextStyle(color = Color(0xFF1393E2))
                     )
-                    Text(text = joke.joke)
+                    Text(text = joke)
                 }
             }
-            Column(modifier = Modifier.align(Alignment.CenterVertically)) {
+
+            Column(modifier = Modifier.align(Alignment.CenterVertically).padding(0.dp,0.dp,10.dp,0.dp)) {
                 Card(
                     modifier = Modifier
                         .size(48.dp)
@@ -90,57 +103,97 @@ fun JokeCellOne(joke: Joke, viewModel: JokesViewModel) {
                     shape = RoundedCornerShape(24.dp),
                 ) {
 
-                    IconButton(modifier = Modifier.padding(defaultWidgetPadding),
+                    //twopart
+                    val isJokeInDatabase = remember { mutableStateOf(false) }
+                    LaunchedEffect(Unit) {
+                        isJokeInDatabase.value = viewModel.isJokeInLocalDatabase(setup + " " + delivery)
+                    }
+
+                    //single
+                    val isJokeInDatabaseSingle = remember { mutableStateOf(false) }
+                    LaunchedEffect(Unit) {
+                        isJokeInDatabaseSingle.value = viewModel.isJokeInLocalDatabase(joke)
+                    }
+
+                    val iconTint = if (type == "twopart") {
+                        if (isJokeInDatabase.value) {
+                            Color.Red
+                        } else {
+                            Color.Black
+                        }
+                    } else {
+                        if (isJokeInDatabaseSingle.value) {
+                            Color.Red
+                        } else {
+                            Color.Black
+                        }
+                    }
+
+                    IconButton(
+                        modifier = Modifier.padding(0.dp),
+
                         onClick = {
-                            if (joke.type == "twopart") {
-                                viewModel.saveJoke(joke.setup+" "+joke.delivery)
-                            }else{
-                                viewModel.saveJoke(joke.joke)
+                            if (type == "twopart") {
+                                if (isJokeInDatabase.value) {
+                                    run { error.addMessage(
+                                        when (savedEmail.value.toString()) {
+                                            "Es" -> "Broma ya guardada"
+                                            "En" -> "Joke already recorded"
+                                            "De" -> "Witz bereits aufgenommen"
+                                            "Fr" -> "Blague enregistrée avec succès"
+                                            else -> {"Joke already recorded"}
+                                        }) }
+                                } else {
+                                    viewModel.saveJoke(setup + " " + delivery)
+                                    run { success.addMessage(
+                                        when (savedEmail.value.toString()) {
+                                            "Es" -> "Broma guardada correctamente"
+                                            "En" -> "Successfully recorded joke"
+                                            "De" -> "Witz erfolgreich aufgenommen"
+                                            "Fr" -> "Blague déjà enregistrée"
+                                            else -> {"Successfully recorded joke"}
+                                        }) }
+                                }
+
+                            } else {
+                                if (isJokeInDatabaseSingle.value) {
+                                    run { error.addMessage(when (savedEmail.value.toString()) {
+                                        "Es" -> "Broma ya guardada"
+                                        "En" -> "Joke already recorded"
+                                        "De" -> "Witz bereits aufgenommen"
+                                        "Fr" -> "Blague enregistrée avec succès"
+                                        else -> {"Joke already recorded"}
+                                    }) }
+                                } else {
+                                    viewModel.saveJoke(joke)
+                                    run { success.addMessage(
+                                        when (savedEmail.value.toString()) {
+                                            "Es" -> "Broma guardada correctamente"
+                                            "En" -> "Successfully recorded joke"
+                                            "De" -> "Witz erfolgreich aufgenommen"
+                                            "Fr" -> "Blague déjà enregistrée"
+                                            else -> {"Successfully recorded joke"}
+                                        }
+                                    ) }
+                                }
+
                             }
-                            snackbarCoroutineScope.launch{
-                                scaffoldState.snackbarHostState.showSnackbar("Snacks in Compose")
-                            }
+
+
                         }
                     ) {
                         Icon(
+                            modifier = Modifier.size(30.dp),
+                            tint = iconTint,
                             painter = painterResource(id = R.drawable.ic_baseline_emoji_favorite),
                             contentDescription = Destinations.Pantalla2.title,
                         )
                     }
+
                 }
+
             }
         }
 
     }
 }
-/*
-
-@Composable
-fun ImageButton(
-    modifier: Modifier,
-    @DrawableRes drawableResId: Int,
-    contentDescription: String = "",
-    onClick: () -> Unit
-) {
-    Card(
-        modifier = modifier
-            .size(48.dp)
-            .neu(
-                lightShadowColor = AppColors.lightShadow(),
-                darkShadowColor = AppColors.darkShadow(),
-                shadowElevation = defaultElevation,
-                lightSource = LightSource.LEFT_TOP,
-                shape = Flat(Oval),
-            ),
-        elevation = 0.dp,
-        shape = RoundedCornerShape(24.dp),
-    ) {
-        Image(
-            modifier = Modifier.clickable(true, onClick = onClick),
-            painter = painterResource(id = drawableResId),
-            contentDescription = contentDescription,
-            contentScale = ContentScale.Inside,
-            colorFilter = ColorFilter.tint(MaterialTheme.colors.primary)
-        )
-    }
-}*/
